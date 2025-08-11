b603b07e3e60dd5bbe89e9da7e359555611a1440
Ali Vafaei <35658253+itsalivafaei@users.noreply.github.com>
2025-08-11 04:21:30 -0400

    Initial Commit

---
+++ translate assist/translate_assistApp.swift
@@ -0,0 +1,17 @@
+//
+//  translate_assistApp.swift
+//  translate assist
+//
+//  Created by monono on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct translate_assistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
